45596158b847cd2c2c89e3562d5b54ceae56c730
Trard <trardone@gmail.com>
2021-09-09 20:23:11 +0300

    add auto-add middlewares

--- src/telegram_bot/middlewares/index.js
+++ src/telegram_bot/middlewares/index.js
@@ -1,3 +1,9 @@
-module.exports = {
-    logger: require('./logger'),
-};
+const fs = require('fs');
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => file != "index.js")
+    .map(file => {
+        let middleware = require(`./${file}`)
+        module.exports[middleware.name] = middleware
+    })
